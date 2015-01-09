2054077077f681c77bcc75fd9123cd8d2fd4ce15
Leo Mdivani <leomdiv@gmail.com>
2015-01-09 14:20:53 +0000

    newmac

--- Source/MainScene.m
+++ Source/MainScene.m
@@ -15,18 +15,9 @@ static const CGFloat scrollSpeed = 80.f;
 
 
 - (void)update:(CCTime)delta {
-    hero.position = ccp(hero.position.x + delta * scrollSpeed, hero.position.y);
-    _physicsNode.position = ccp(_physicsNode.position.x - (scrollSpeed *delta), _physicsNode.position.y);
-    // loop the ground
-    for (CCNode *background in _backgrounds) {
-        // get the world position of the ground
-        CGPoint backgroundWorldPosition = [_physicsNode convertToWorldSpace:background.position];
-        // get the screen position of the ground
-        CGPoint backgroundScreenPosition = [self convertToNodeSpace:backgroundWorldPosition];
-        // if the left corner is one complete width off the screen, move it to the right
-        if (backgroundScreenPosition.x <= (-1 * background.contentSize.width)) {
-            background.position = ccp(background.position.x + 2 * background.contentSize.width, background.position.y);
-        }
-    }
+    //hero.position = ccp(hero.position.x + delta * scrollSpeed, hero.position.y);
+    //_physicsNode.position = ccp(_physicsNode.position.x, _physicsNode.position.y  - (scrollSpeed *delta));
 }
+
+
 @end
