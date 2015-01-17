83fda22109a92778e471055a01520b785f15dde7
leomdivani <leomdiv@gmail.com>
2015-01-17 14:09:28 +0000

    highscore working properly

--- GameOverScene.m
+++ GameOverScene.m
@@ -2,11 +2,21 @@
 
 @implementation GameOverScene{
     CCButton *_restartButton;
+    int _highScore;
+    CCLabelTTF *_highScoreLabel;
 }
+- (void)didLoadFromCCB {
+    [self loadHighScore];
 
+}
 -(void)restartButtonClicked {
     CCScene *scene = [CCBReader loadAsScene:@"MainScene"];
     [[CCDirector sharedDirector] replaceScene:scene withTransition:[CCTransition transitionPushWithDirection:CCTransitionDirectionLeft duration:0.25f]];
 }
-
+- (void)loadHighScore {
+    
+    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+    _highScore = [prefs integerForKey:@"highScore"];
+    _highScoreLabel.string = [NSString stringWithFormat:@"High Score: %ld",(long)_highScore];
+}
 @end
