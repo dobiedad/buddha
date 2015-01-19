6875670bbdf3178e1e9f9eca62ef49ba7117fb5e
leomdivani <leomdiv@gmail.com>
2015-01-19 14:59:11 +0000

    accelerometer

--- GameOverScene.m
+++ GameOverScene.m
@@ -1,22 +1,52 @@
 #import "GameOverScene.h"
-
+#import <CoreMotion/CoreMotion.h>
 @implementation GameOverScene{
     CCButton *_restartButton;
-    int _highScore;
-    CCLabelTTF *_highScoreLabel;
+    CMMotionManager *_motionManager;
+    CCLabelTTF *_label;
 }
-- (void)didLoadFromCCB {
-    [self loadHighScore];
-
+- (id)init
+{
+    if (self = [super init])
+    {
+        _label= [CCLabelTTF labelWithString:@"X" fontName:@"Arial" fontSize:48];
+        [self addChild:_label];
+        _motionManager = [[CMMotionManager alloc] init];
+    }
+    return self;
 }
 -(void)restartButtonClicked {
     CCScene *scene = [CCBReader loadAsScene:@"MainScene"];
     [[CCDirector sharedDirector] replaceScene:scene withTransition:[CCTransition transitionPushWithDirection:CCTransitionDirectionLeft duration:0.25f]];
 }
-- (void)loadHighScore {
+- (void)onEnter
+{
+    [super onEnter];
     
-    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
-    _highScore = [prefs integerForKey:@"highScore"];
-    _highScoreLabel.string = [NSString stringWithFormat:@"High Score: %ld",(long)_highScore];
+ CGSize winSize = [CCDirector sharedDirector].viewSize;
+ CGPoint midpoint = ccp(winSize.width/2, winSize.height/2);
+    
+    _label.position = midpoint;
+    
+    [_motionManager startAccelerometerUpdates];
 }
+- (void)onExit
+{
+    [super onExit];
+    [_motionManager stopAccelerometerUpdates];
+}
+
+- (void)update:(CCTime)delta {
+    CMAccelerometerData *accelerometerData = _motionManager.accelerometerData;
+    CMAcceleration acceleration = accelerometerData.acceleration;
+    
+    CGSize winSize = [CCDirector sharedDirector].viewSize;
+    
+    CGFloat newXPosition = _label.position.x + acceleration.x * (1000 * delta);
+    
+    newXPosition = clampf(newXPosition, 0, winSize.width);
+    
+    _label.position = CGPointMake(newXPosition, _label.position.y);
+}
+
 @end
